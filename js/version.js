8a3247596c478e976e20d932fd1fab8d9f12d8d4
nickpeets <nickpeets@gmail.com>
2026-07-03 19:36:28 -0700

    Bump js/version.js build hash to 9b3eb1c (catch up after PR #47)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = '20141f3';
+  var BUILD = '9b3eb1c';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
