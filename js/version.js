b655ff1dc8eb0d613ec4b4f4f5a0c7d99f3695ad
nickpeets <nickpeets@gmail.com>
2026-07-04 15:32:37 -0700

    Bump js/version.js to 89dfb11 (game-shell scope-reassert fix merge)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = '0264304';
+  var BUILD = '89dfb11';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
