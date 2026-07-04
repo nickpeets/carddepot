06a70b9ecd7dfabf79a5155205a5a94f46dd11fe
nickpeets <nickpeets@gmail.com>
2026-07-03 21:28:28 -0700

    Bump js/version.js to f680863 (Session 1 foundation merge)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = '27670ff';
+  var BUILD = 'f680863';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
