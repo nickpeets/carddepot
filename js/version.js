4caa937252481f553714991f9e17d88210c6c61c
nickpeets <nickpeets@gmail.com>
2026-07-04 08:45:42 -0700

    Bump js/version.js to 1015ff0 (Season FOUC fix merge)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = '4f174b4';
+  var BUILD = '1015ff0';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
