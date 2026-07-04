569f5f90ebe59129e801c63a77f09df9b31fad9a
nickpeets <nickpeets@gmail.com>
2026-07-03 18:45:36 -0700

    Bump js/version.js build hash to 20141f3

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = 'PENDING-version-js';
+  var BUILD = '20141f3';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
