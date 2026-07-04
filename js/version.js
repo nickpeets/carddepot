15614f932ca79d59af56937e6384c158ca40c49a
nickpeets <nickpeets@gmail.com>
2026-07-03 23:31:45 -0700

    Bump js/version.js to 94ff9b7 (Session 3 season screen merge)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = '0f5e8fd';
+  var BUILD = '94ff9b7';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
