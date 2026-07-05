abdd1ff657126d6d5f1694610b1a2964bcaac766
nickpeets <nickpeets@gmail.com>
2026-07-04 21:44:33 -0700

    version.js: bump build to 81d2ae6 (PR #97 merge)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = '2428310';
+  var BUILD = '81d2ae6';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
