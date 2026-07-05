cf4ad945bf44afa1cd565c2400c6099961076d9c
nickpeets <nickpeets@gmail.com>
2026-07-04 21:25:59 -0700

    version.js: bump build to 2428310 (PR #95 merge)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = 'a125075';
+  var BUILD = '2428310';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
