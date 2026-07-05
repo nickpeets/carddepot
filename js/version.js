5c99cfb221b38b0597dd945da1cce63e23331d5c
nickpeets <nickpeets@gmail.com>
2026-07-05 16:36:39 -0700

    chore(version): bump build to f32ad80 (Slice A wallet + payout)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = '50d695d';
+  var BUILD = 'f32ad80';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
