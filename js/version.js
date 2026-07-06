323e51ed28bda9371c108d51e730e650ce79034d
nickpeets <nickpeets@gmail.com>
2026-07-05 17:17:05 -0700

    chore(version): bump build to 781acf3 (observer-loop fix)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = 'f32ad80';
+  var BUILD = '781acf3';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
