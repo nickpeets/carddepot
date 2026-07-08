0b458321d355e28a6ac0f3616a88c536379d6569
nickpeets <nickpeets@gmail.com>
2026-07-08 14:39:30 -0700

    chore(version): bump build to 11b92ef (PR #117 merge)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = 'dd07f87';
+  var BUILD = '11b92ef';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
