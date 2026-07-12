7d73396e322f2285e66139203c62197cb775f757
nickpeets <nickpeets@gmail.com>
2026-07-12 13:51:32 -0700

    Update BUILD variable to new commit hashchore: bump build to 825c610 (deploy Stage 3 free-pack odds fix - band-first draw)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = '7c13ab2';
+  var BUILD = '825c610';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
