c24192fbc10a214aa2666f14d7818802aefcb6bf
nickpeets <nickpeets@gmail.com>
2026-07-12 02:25:28 +0000

    chore(version): bump build to 1e9c820 (shop screen + tier expansion deploy)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = '7093a46';
+  var BUILD = '1e9c820';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
