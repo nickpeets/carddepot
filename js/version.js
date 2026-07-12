566ce3fec7abe04bf587a6c772c10eadfed1ecaf
nickpeets <nickpeets@gmail.com>
2026-07-12 11:47:43 -0700

    chore: bump build to 163f222 (deploy season-header-hybrid PR #130)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = '2bf42c0';
+  var BUILD = '163f222';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
