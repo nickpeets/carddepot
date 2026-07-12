a4349f7304a126caf9198a3c2839a441cfe42ed5
nickpeets <nickpeets@gmail.com>
2026-07-11 20:45:41 -0700

    Update BUILD variable to new cochore: bump build marker to 93cb117 (shop auth-settle fix)mmit short-hash

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = '834734c';
+  var BUILD = '93cb117';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
