5c09aa786090cad30d8abb8739c4464d6aa21744
nickpeets <nickpeets@gmail.com>
2026-07-12 13:24:59 -0700

    chore: bump build to 7c13ab2 (deploy Stage 3 FREE DAILY pack + pixel-front render integration)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = '163f222';
+  var BUILD = '7c13ab2';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
