6307ae614fc5a8f62f83ec63bc4942c11f10eb41
nickpeets <nickpeets@gmail.com>
2026-07-12 16:32:48 -0700

    chore(deploy): bump BUILD to 737b9eb (in-binder shop + reveal ceremony + native pixel front)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = '825c610';
+  var BUILD = '737b9eb';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
