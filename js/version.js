96d490c9ab7ce9ad3125e9587b9af3f6d786e7b4
nickpeets <nickpeets@gmail.com>
2026-07-12 16:53:53 -0700

    chore(deploy): bump BUILD to 55f832a (shop catalog path fix)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = '737b9eb';
+  var BUILD = '55f832a';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
