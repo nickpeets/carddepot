13c9921822ba3597d36af360249596e61b43955c
nickpeets <nickpeets@gmail.com>
2026-07-13 05:44:57 +0000

    chore(deploy): label build d1db3c2 (branch-tip) across version.js + all ?v= queries

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD = '47b880e';
+  var BUILD = 'd1db3c2';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
