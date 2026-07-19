df8a2cab805873838c6dc5d42cd23e97bc98f7e8
nickpeets <nickpeets@gmail.com>
2026-07-19 22:19:57 +0000

    chore: cache-bust to 0f70871 (version.js BUILD + all ?v= across four shells)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD='b101db0';
+  var BUILD='0f70871';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
