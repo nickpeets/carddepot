83a09897d4123a9246a87f2040a317ea1c78cf41
nickpeets <nickpeets@gmail.com>
2026-07-20 12:41:21 -0700

    Cache-bust: version.js BUILD -> 4436684

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD='0d88c68';
+  var BUILD='4436684';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
