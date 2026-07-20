cd2924348e8fb354c942d2ae1b85e4c75e887015
nickpeets <nickpeets@gmail.com>
2026-07-19 21:48:16 -0700

    chore: bump version.js BUILD to 59ef110 (branch-tip label)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD='9ce1034';
+  var BUILD='59ef110';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
