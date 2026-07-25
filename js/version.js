d4a845ae9492cba2faa28ab088dfde7ed0d28d73
nickpeets <nickpeets@gmail.com>
2026-07-25 23:33:12 +0000

    chore(cache-bust): stamp BUILD + all ?v= to bb7644d (§6 branch-tip label)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD='be36d25';
+  var BUILD='bb7644d';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
