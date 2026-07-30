ffb612c967441512ca80cd8cbac572d240742ebd
nickpeets <nickpeets@gmail.com>
2026-07-30 18:13:47 +0000

    chore: cache-bust to 790e076 (62 tags, BUILD=790e076)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD='8522149';
+  var BUILD='790e076';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
