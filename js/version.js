daddc2ef681c3fd9a5101e36bf8f8382b14ca227
nickpeets <nickpeets@gmail.com>
2026-08-01 05:37:31 +0000

    chore(cache-bust): stamp BUILD + all 80 ?v= tags to substantive 22ddb34 (index 29, shop 18, game 12, builder 12, preview 1, market 8)

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD='4d4042f';
+  var BUILD='22ddb34';
   window.DEPOT_BUILD = BUILD;
   try {
     console.log('[depot] build ' + BUILD);
