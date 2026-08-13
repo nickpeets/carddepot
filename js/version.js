baee2fc678673f2e88116c4f9c81213836cab3a6
nickpeets <nickpeets@gmail.com>
2026-08-13 20:48:07 +0000

    chore(cache-bust): restamp 120 tags across 10 shells to 3e89420 - welcome.html adds 8

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD='c282bd9';
+  var BUILD='3e89420';
   window.DEPOT_BUILD = BUILD;
   try {
     (window.depotLog||function(){})('[depot] build ' + BUILD);
