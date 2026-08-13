88d228f8a8ba5caeff98d85f206ad5807b006398
nickpeets <nickpeets@gmail.com>
2026-08-13 21:28:42 +0000

    chore(cache-bust): restamp 120 tags across 10 shells after ch20 slice

--- js/version.js
+++ js/version.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Deployed commit short-hash. Bumped on every merge (AGENTS.md section 5).
-  var BUILD='3e89420';
+  var BUILD='9fdf448';
   window.DEPOT_BUILD = BUILD;
   try {
     (window.depotLog||function(){})('[depot] build ' + BUILD);
